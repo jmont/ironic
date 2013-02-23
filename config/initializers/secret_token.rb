e443b989e6ae390401b0efbc646f3ed8df5e6174
Juan Carlos Montemayor Elosua <j.mont@me.com>
2013-02-23 14:07:49 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ironic::Application.config.secret_token = '03628250882a0ee1005bea7424e21d376ea80e9e1463d60447ce312953dc57bfcbd090e50edeaaf9d3beaf91d702bf0e9f1945d03645b9eac140c5ddbcbd311e'
