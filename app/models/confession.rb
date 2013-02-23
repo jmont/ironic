42815381ed5a248bf37680e62b1f7440fc5e10e8
Juan Carlos Montemayor Elosua <j.mont@me.com>
2013-02-23 14:30:28 -0500

    added comments to confessions

--- app/models/confession.rb
+++ app/models/confession.rb
@@ -1,3 +1,4 @@
 class Confession < ActiveRecord::Base
+  has_many :comments
   attr_accessible :txt
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130223192147) do
+ActiveRecord::Schema.define(:version => 20130223192657) do
+
+  create_table "comments", :force => true do |t|
+    t.text     "txt"
+    t.integer  "confession_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
 
   create_table "confessions", :force => true do |t|
     t.text     "txt"
