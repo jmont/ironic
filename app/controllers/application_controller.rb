551d1e6e90da2784c40fb08cea144d400c80fcfa
Amanda Hay <amanda.hay@tufts.edu>
2013-02-27 21:27:20 -0500

    testing devise on main page --hopefully no massive mem leaks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :log_ram # or use after_filter
-  def log_ram
-    logger.warn 'RAM USAGE: ' + `pmap #{Process.pid} | tail -1`[10,40].strip
-  end
+  #before_filter :log_ram # or use after_filter
+  #def log_ram
+  #  logger.warn 'RAM USAGE: ' + `pmap #{Process.pid} | tail -1`[10,40]
+  #end
 end
