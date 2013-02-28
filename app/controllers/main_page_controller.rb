8da03a5dbf3b38b5f0acf7b7792769cb67c650f4
Amanda Hay <amanda.hay@tufts.edu>
2013-02-28 12:38:02 -0500

    removed output from funciton calls

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -2,6 +2,12 @@ class MainPageController < ApplicationController
 
   def index
     @confessions = Confession.paginate(:page => params[:page], :per_page => 10)
+    logger.debug @confessions.first
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @confessions }
+    end
   end
 
 end
