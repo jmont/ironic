bcc60347c78227b16f2f9090a29eed6e8b59a193
Amanda Hay <amanda.hay@tufts.edu>
2013-02-28 16:42:31 -0500

    changed heroku url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Ironic::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'evening-lake-4680.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'tuftsconfess.herokuapp.com' }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
