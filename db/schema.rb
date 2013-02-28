fa6c8759a1f500bdcb4c64fb81f07842725399b3
Amanda Hay <amanda.hay@tufts.edu>
2013-02-28 17:08:17 -0500

    blah

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,37 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130223192657) do
+ActiveRecord::Schema.define(:version => 20130226035528) do
+
+  create_table "accounts", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+  end
+
+  add_index "accounts", ["email"], :name => "index_accounts_on_email", :unique => true
+  add_index "accounts", ["reset_password_token"], :name => "index_accounts_on_reset_password_token", :unique => true
 
   create_table "comments", :force => true do |t|
     t.text     "txt"
     t.integer  "confession_id"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+    t.integer  "flags"
   end
 
   create_table "confessions", :force => true do |t|
     t.text     "txt"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "flags"
   end
 
 end
