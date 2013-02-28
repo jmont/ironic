9ef78238cfb4f3ceecef37522c3007ce54097ef9
Amanda Hay <amanda.hay@tufts.edu>
2013-02-28 11:31:05 -0500

    fixed nil problem

--- app/helpers/confessions_helper.rb
+++ app/helpers/confessions_helper.rb
@@ -2,6 +2,7 @@ module ConfessionsHelper
   def view_cookie(id)
     name = 'confession' + id.to_s()
     if !cookies[:name].present? or cookies[:name] == "0"
+      cookies[:name] = 0
       "Flag" + cookies[:name] + name
     else
       "Unflag" + cookies[:name] + name
