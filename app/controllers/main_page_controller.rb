88fe8084b800db1cec5579203ca4500d802ba7fc
Amanda Hay <amanda.hay@tufts.edu>
2013-02-28 18:11:25 -0500

    conf pages work again

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -2,12 +2,6 @@ class MainPageController < ApplicationController
 
   def index
     @confessions = Confession.paginate(:page => params[:page], :per_page => 20)
-    logger.debug @confessions.first
-    
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @confessions }
-    end
   end
 
 end
